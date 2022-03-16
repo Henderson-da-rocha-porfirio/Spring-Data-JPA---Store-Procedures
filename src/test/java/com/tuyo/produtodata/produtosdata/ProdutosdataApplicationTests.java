278690a3dbb0e38cd1c5cba74abe155c9af96944
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-16 18:10:02 -0300

    # commit 6 - master - correndo JUnit testCreate

--- src/test/java/com/tuyo/produtodata/produtosdata/ProdutosdataApplicationTests.java
+++ src/test/java/com/tuyo/produtodata/produtosdata/ProdutosdataApplicationTests.java
@@ -1,7 +1,10 @@
 package com.tuyo.produtodata.produtosdata;
 
+import com.tuyo.produtodata.produtosdata.entities.Produto;
+import com.tuyo.produtodata.produtosdata.repository.ProdutoRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,22 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 class ProdutosdataApplicationTests { 	// Junit testes
 
+	@Autowired
+	ProdutoRepository repository;
+
 	@Test 								// Construtor marcado com a annotation Test.
 	void contextLoads() {				// Esse teste ele vai correr e procurar por uma classe que tiver marcada, no classpath, com a @SprinBootApplication.
+	}
+
+	@Test
+	void testCreate() {
+		Produto produto = new Produto();
+		produto.setId(1);
+		produto.setName("Iphone");
+		produto.setDesc("Awesome");
+		produto.setPrice(1000d);
+
+		repository.save(produto);
 	}
 
 }
