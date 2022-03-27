9487c19022b92cf99038ad4220d7201475c10cee
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-03-27 09:13:54 -0300

    Update ProdutosdataApplicationTests.java

--- src/test/java/com/tuyo/produtodata/produtosdata/ProdutosdataApplicationTests.java
+++ src/test/java/com/tuyo/produtodata/produtosdata/ProdutosdataApplicationTests.java
@@ -166,17 +166,3 @@ class ProdutosdataApplicationTests {    // Junit testes
         repository.findAll(pageable).forEach(p -> System.out.println(p.getName()));
 
     }
-
-/*    @Test
-    @Transactional
-    public void testCaching() {
-        Session session = entityManager.unwrap(Session.class);
-        Produto produto = repository.findById(1).get();
-
-        repository.findById(1).get();
-
-        session.evict(produto);
-
-        repository.findById(1).get();
-    }*/
-}
